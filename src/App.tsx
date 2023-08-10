1315d7706729ba34b3ad5594ccea012bac100ad7
Piotr Ostrowski <piotrostr@google.com>
2023-08-10 11:11:54 +0200

    experiment with the plain, add zoom-in and zoom-out using react-zoom-pan-pinch-setscale

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useControls } from "react-zoom-pan-pinch";
+import { ReactZoomPanPinchState } from "react-zoom-pan-pinch";
+import { useTransformContext } from "react-zoom-pan-pinch";
+import { useTransformEffect } from "react-zoom-pan-pinch";
+import { ReactZoomPanPinchHandlers } from "react-zoom-pan-pinch";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <TransformWrapper
+      initialScale={0.3}
+      limitToBounds={false}
+      centerZoomedOut={false}
+      disablePadding={true}
+    >
+      <React.Fragment>
+        <TransformComponent>
+          <div
+            style={{
+              width: "100vw",
+              height: "100vh",
+              background: "red",
+            }}
+          />
+          <div>Example text</div>
+        </TransformComponent>
+        <Sidebar />
+      </React.Fragment>
+    </TransformWrapper>
+  );
 }
 
-export default App
+function Sidebar() {
+  const { setTransform, resetTransform } = useControls();
+  const [state, setState] = useState<ReactZoomPanPinchState>({
+    positionX: 0,
+    positionY: 0,
+    scale: 1,
+    previousScale: 1,
+  });
+
+  const context = useTransformContext();
+  context.onWheelZoom = (e) => {
+    handleScroll(e);
+  };
+
+  const handleZoomIn = (step: number = 0.1) => {
+    setTransform(state.positionX, state.positionY, state.scale + step);
+  };
+
+  const handleZoomOut = (step: number = 0.1) => {
+    setTransform(state.positionX, state.positionY, state.scale - step);
+  };
+
+  const handleReset = () => {
+    resetTransform();
+  };
+
+  const handleScroll = (e) => {
+    console.log(e);
+    e.preventDefault();
+    e.stopPropagation();
+    const { deltaY } = e;
+    const { scale } = state;
+    const newScale = scale - deltaY * 0.01;
+    setTransform(state.positionX, state.positionY, newScale);
+  };
+
+  useTransformEffect(({ state, instance }) => {
+    setState(state);
+    return () => {};
+  });
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        top: 50,
+        left: 50,
+        zIndex: 100,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <button onClick={() => handleZoomIn()}>+</button>
+      <button onClick={() => handleZoomOut()}>-</button>
+      <button onClick={() => handleReset()}>x</button>
+    </div>
+  );
+}
+
+export default App;
